90597eeec7446426b5fea8f5a21bd8a9d2d81da4
Arman Mohammadi <rman.mo2000@gmail.com>
2020-11-25 15:52:02 +0330

    night mode handled for main section

---
+++ scripts.js
@@ -0,0 +1,50 @@
+let night_mode = 'day';
+let sidebar_status = 'close';
+
+window.transitionToPage = function (href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function () {
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    setTimeout(function () {
+        document.querySelector('body').style.opacity = 1
+    }, 0)
+})
+
+function change_light_mode() {
+    if (night_mode === 'day') {
+        night_mode = 'night';
+        document.getElementById("sidebar").classList.add("night-mode");
+        document.getElementById('main').classList.add("night-mode");
+        cards = document.getElementsByClassName('card')
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].classList.add('night-mode')
+        }
+    } else {
+        night_mode = 'day';
+        document.getElementById("sidebar").classList.remove("night-mode");
+        document.getElementById('main').classList.remove("night-mode");
+        cards = document.getElementsByClassName('card')
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].classList.remove('night-mode')
+        }
+    }
+}
+
+function change_sidebar_status() {
+    if (sidebar_status === 'close') {
+        sidebar_status = 'open';
+        document.getElementById("sidebar").style.left = '0px';
+        document.getElementById("sidebar-close-btn").style.display = 'inline';
+        document.getElementById("sidebar-open-btn").style.display = 'none';
+
+    } else {
+        sidebar_status = 'close';
+        document.getElementById("sidebar").style.left = '-1000px';
+        document.getElementById("sidebar-close-btn").style.display = 'none';
+        document.getElementById("sidebar-open-btn").style.display = 'inline';
+    }
+}
